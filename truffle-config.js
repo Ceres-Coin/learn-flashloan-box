0da88fdeb325a5fbd027d5dc0124fe240f5dfee9
IlyaPrigogine <prigogine@protonmail.com>
2021-09-15 15:28:07 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,8 @@ module.exports = {
 	  development: {
 	    host: "127.0.0.1",
 	    port: 8545,
-	    // gas: 20000000,
+	    gas: 20000000,
+		gasPrice: 100000000000,
 	    network_id: "*",
 	    skipDryRun: true
 	  },
